e463c02cd3ddff6c0d1b5adc2e4fe08dc36afc14
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2015-10-27 03:12:01 -0500

    wrote adapter for ContactsList ListView

--- app/src/main/java/contactskeeper/achaturvedi/com/contactskeeper/ContactDataAdapter.java
+++ app/src/main/java/contactskeeper/achaturvedi/com/contactskeeper/ContactDataAdapter.java
@@ -1,7 +1,37 @@
 package contactskeeper.achaturvedi.com.contactskeeper;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Akash on 10/27/2015.
  */
-public class ContactDataAdapter {
+public class ContactDataAdapter extends ArrayAdapter{
+
+    private final Context context;
+    private final ArrayList<ContactDataModel> contactDataModelArrayList;
+    TextView fullName, phoneNum;
+    public ContactDataAdapter(Context context, ArrayList<ContactDataModel> contactDataModelArrayList) {
+        super(context, R.layout.contacts_element, contactDataModelArrayList);
+        this.context = context;
+        this.contactDataModelArrayList = contactDataModelArrayList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.contacts_element, parent, false);
+        }
+
+        fullName = (TextView)convertView.findViewById(R.id.fullName);
+        phoneNum = (TextView)convertView.findViewById(R.id.phoneNum);
+        return convertView;
+    }
 }
